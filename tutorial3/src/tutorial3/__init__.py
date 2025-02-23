a82000830527396c6e195bb3f46a94a4001f7256
Matt Fisher <mfisher87@gmail.com>
2025-02-23 11:42:22 -0700

    Refactor simulation update logic into Simulation class

--- tutorial3/src/tutorial3/__init__.py
+++ tutorial3/src/tutorial3/__init__.py
@@ -17,12 +17,22 @@ class Simulation:
         ]
         self._last_time = time.time()
 
-    @property
-    def balls(self):
-        return self._balls
+    def update(self, sketch_step):
+        mouse = sketch_step.get_mouse()
+        mouse_x = mouse.get_pointer_x()
+        mouse_y = mouse.get_pointer_y()
 
+        self._update_balls(mouse_x, mouse_y)
 
-    def update(self, mouse_x, mouse_y):
+        for ball in self._balls:
+            sketch_step.draw_ellipse(
+                ball.position_x,
+                ball.position_y,
+                BALL_DIAMETER,
+                BALL_DIAMETER,
+            )
+
+    def _update_balls(self, mouse_x, mouse_y):
         new_time = time.time()
         duration = new_time - self._last_time
         self._last_time = new_time
@@ -35,22 +45,7 @@ def main():
     sketch = sketchingpy.Sketch2D(WIDTH, HEIGHT)
     simulation = Simulation()
 
-    def update_and_draw_balls(self):
-        mouse = sketch.get_mouse()
-        mouse_x = mouse.get_pointer_x()
-        mouse_y = mouse.get_pointer_y()
-
-        simulation.update(mouse_x, mouse_y)
-
-        for ball in simulation.balls:
-            sketch.draw_ellipse(
-                ball.position_x,
-                ball.position_y,
-                BALL_DIAMETER,
-                BALL_DIAMETER,
-            )
-
-    sketch.on_step(update_and_draw_balls)
+    sketch.on_step(simulation.update)
     sketch.show()
 
 
